215347e4fe1914ace29ae515810ca86f673e2870
The-Tech-Revolutioner <75535904+The-Tech-Revolutioner@users.noreply.github.com>
2020-12-22 17:00:53 +0000

    Update drmon.lua

--- drmon.lua
+++ drmon.lua
@@ -11,7 +11,7 @@ local identify = false
 -- please leave things untouched from here on
 os.loadAPI("lib/f")
 
-local version = "1.0"
+local version = "1.0.0"
 -- toggleable via the monitor, use our algorithm to achieve our target field strength or let the user tweak it
 local autoInputGate = 1
 
