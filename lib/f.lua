2ad5fc8eeae5eb9f730623e5cf35b4d6f5e5ab33
The-Tech-Revolutioner <75535904+The-Tech-Revolutioner@users.noreply.github.com>
2020-12-22 16:07:44 +0000

    Add periphSearch from OmegaPhi115

--- lib/f.lua
+++ lib/f.lua
@@ -1,3 +1,17 @@
+
+-- peripheral identification
+--
+function periphSearch(type)
+   local names = peripheral.getNames()
+   local i, name
+   for i, name in pairs(names) do
+      if peripheral.getType(name) == type then
+         return peripheral.wrap(name)
+      end
+   end
+   return null
+end
+
 -- formatting
 
 function format_int(number)
